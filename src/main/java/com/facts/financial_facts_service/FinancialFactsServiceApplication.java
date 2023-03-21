2f91ab29981eae0dffb1108106c521317debda16
Choochera <gabrielmatthew20@gmail.com>
2023-03-21 15:57:45 -0500

    Initialize spring boot project

---
+++ src/main/java/com/facts/financial_facts_service/FinancialFactsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.facts.financial_facts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialFactsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialFactsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facts/financial_facts_service/FinancialFactsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facts.financial_facts_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialFactsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
